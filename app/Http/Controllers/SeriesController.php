82dc1a7278119cff409302fee3365ff530f48b7d
Seb <sebrao@live.co.uk>
2017-10-03 23:59:58 +0100

    [+] Added boilerplate for SeriesController

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Series;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        // Return all series with pagination
+    }
+}
