35252d7e2631365dd9bdfa3311b7230ab88df5aa
Seb <sebrao@live.co.uk>
2017-11-20 10:56:35 +0000

    [+] Endpoint for adding a new series, validates req, checks if the entry doesn't already exist and fetches the data

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,11 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Series;
 use Illuminate\Http\Request;
+use App\Library\OmdbApi;
 
 class SeriesController extends Controller
 {
     public function index()
     {
         // Return all series with pagination
+    }
+
+    public function add()
+    {
+        $validatedRequest = request()->validate([
+            'imdbId' => 'required|string'
+        ]);
+
+        $imdbId = $validatedRequest['imdbId'];
+        
+        $series = Series::where('imdbId', $imdbId)->first();
+
+        if ($series) {
+            return response()->json(['error' => 'Series already added'], 409);
+        }
+
+        $omdb = new OmdbApi($imdbId);
+
+        if (!$omdb->addNewSeries()) {
+            response()->json(['error' => 'Something went wrong'], 500);
+        }
+
+        return response()->json(['message' => 'Successfully added to database!'], 200);
     }
 }
