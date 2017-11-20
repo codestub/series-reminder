bfb9e839d9b14493c889dc589db3caba1d5b4ef8
Seb <sebrao@live.co.uk>
2017-11-20 10:55:34 +0000

    [+] Added a CustomException class which renders errors as a json response. I am having doubts about returning response. Maybe should just return the info and let controller handle the errors...

---
+++ app/Exceptions/CustomException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class CustomException extends Exception 
+{
+    public $message;
+    public $code;
+
+    public function __construct($message, $code)
+    {
+        $this->message = $message;
+        $this->code = $code;
+    }
+
+    public function render()
+    {
+        return response()->json(['error' => $this->message], $this->code);
+    }
+}
